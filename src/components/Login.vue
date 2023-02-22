0ef002ed2fe4bfb97b1a926e190ce90e561a186c
Daniel Renteria <jose.r.renteria@oracle.com>
2023-02-22 09:44:16 -0600

    Created login page

---
+++ src/components/Login.vue
@@ -0,0 +1,49 @@
+<script>
+export default {
+  data() {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    onSubmit() {
+      // ...
+      console.log("Submited")
+    },
+  }
+}
+</script>
+
+<template>
+  <main>
+    <van-form @submit="onSubmit">
+      <van-cell-group inset>
+        <van-field
+          v-model="username"
+          name="Username"
+          label="Username"
+          placeholder="Username"
+          :rules="[{ required: true, message: 'Username is required' }]"
+        />
+        <van-field
+          v-model="password"
+          type="password"
+          name="Password"
+          label="Password"
+          placeholder="Password"
+          :rules="[{ required: true, message: 'Password is required' }]"
+        />
+      </van-cell-group>
+      <div style="margin: 16px;">
+        <van-button round block type="primary" native-type="submit">
+          Submit
+        </van-button>
+      </div>
+    </van-form>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,27 @@
 import { createApp } from 'vue'
+import Login from './components/Login.vue'
+
 import App from './App.vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
+import { Form, Field, CellGroup, Button } from 'vant';
 
-import './assets/main.css'
+import 'vant/lib/index.css';
 
-createApp(App).mount('#app')
+
+const routes = [
+    { path: '/', component: Login },
+]
+
+const router = createRouter({
+    // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+    history: createWebHashHistory(),
+    routes, // short for `routes: routes`
+})
+
+const app = createApp(App)
+app.use(router)
+app.use(Form);
+app.use(Field);
+app.use(CellGroup);
+app.use(Button);
+app.mount('#app')
